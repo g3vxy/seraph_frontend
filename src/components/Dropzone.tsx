d06874536eed631b09310608c028a316357963dd
Anıl Berke Sağlam <g3vxyv@gmail.com>
2021-12-27 16:59:33 +0300

    refactor(wasm)

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -2,10 +2,19 @@ import { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { motion } from "framer-motion";
 import useImageStore from "../stores/ImageStore";
+import useWasm from "../hooks/useWasm";
 
 function Dropzone() {
   const [files, setFiles] = useState([]);
-  const { setImages } = useImageStore();
+  const { images, setImages } = useImageStore();
+  const { error, loading, initialize, wasmInstance } = useWasm();
+
+  useEffect(() => {
+    async function loadWasm() {
+      await initialize();
+    }
+    loadWasm();
+  }, []);
 
   const onDrop = useCallback(acceptedFiles => {
     setFiles(
@@ -51,46 +60,65 @@ function Dropzone() {
     [files]
   );
 
+  if (loading) return <>Loading</>;
+  if (error) {
+    return <>"An error has occurred"</>;
+  }
+
   return (
     // @ts-ignore
-    <motion.div
-      className='flex justify-center mt-4 w-full'
-      transition={{
-        type: "spring"
-      }}
-      initial={{ opacity: 0, translateY: -20 }}
-      animate={{ opacity: 1, translateY: 0 }}
-      whileHover={{ scale: 1.1 }}
-      {...getRootProps({ onClick: evt => evt.preventDefault() })}>
-      <div className='rounded-lg shadow-xl bg-gray-50 dark:bg-gray-800 w-2/3'>
-        <div className='m-4'>
-          <div className='flex items-center justify-center w-full'>
-            <label className='flex flex-col w-full h-32 border-4 border-dashed hover:bg-gray-100 dark:hover:bg-gray-700 hover:border-gray-300 cursor-pointer'>
-              <div className='flex flex-col items-center justify-center pt-7'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='w-12 h-12 text-gray-400 group-hover:text-gray-600'
-                  viewBox='0 0 20 20'
-                  fill='currentColor'>
-                  <path
-                    fillRule='evenodd'
-                    d='M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z'
-                    clipRule='evenodd'
-                  />
-                </svg>
-                <p className='pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600'>
-                  Select a photo
-                </p>
-              </div>
-              <input type='file' className='opacity-0' {...getInputProps()} />
-            </label>
+    <>
+      <motion.div
+        className='flex justify-center mt-4 w-full'
+        transition={{
+          type: "spring"
+        }}
+        initial={{ opacity: 0, translateY: -20 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        whileHover={{ scale: 1.1 }}>
+        <div className='rounded-lg shadow-xl bg-gray-50 dark:bg-gray-800 w-2/3'>
+          <div className='m-4'>
+            <div className='flex items-center justify-center w-full'>
+              <label
+                className='flex flex-col w-full h-32 border-4 border-dashed hover:bg-gray-100 dark:hover:bg-gray-700 hover:border-gray-300 cursor-pointer'
+                {...getRootProps({ onClick: evt => evt.preventDefault() })}>
+                <div className='flex flex-col items-center justify-center pt-7'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='w-12 h-12 text-gray-400 group-hover:text-gray-600'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'>
+                    <path
+                      fillRule='evenodd'
+                      d='M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                  <p className='pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600'>
+                    Select a photo
+                  </p>
+                </div>
+                <input type='file' className='opacity-0' {...getInputProps()} />
+              </label>
+            </div>
+          </div>
+          <div className='flex flex-row flex-wrap justify-center gap-4 px-8 pb-4'>
+            {thumbs}
           </div>
         </div>
-        <div className='flex flex-row flex-wrap justify-center gap-4 px-8 pb-4'>
-          {thumbs}
-        </div>
-      </div>
-    </motion.div>
+      </motion.div>
+      <button
+        className='mt-8 bg-gray-600 hover:bg-gray-800 dark:bg-gray-400 hover:dark:bg-gray-200 text-white dark:text-gray-100 hover:dark:text-gray-600 py-2 px-4 rounded'
+        onClick={() => {
+          try {
+            wasmInstance?.read_images_web(images[0]);
+          } catch (error) {
+            console.error(error);
+          }
+        }}>
+        Test
+      </button>
+    </>
   );
 }
 
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,26 +1,8 @@
 import { motion } from "framer-motion";
-import { useEffect } from "react";
 import Dropzone from "../components/Dropzone";
 import Header from "../components/Header";
-import useWasm from "../hooks/useWasm";
-import useImageStore from "../stores/ImageStore";
 
 function LandingPage() {
-  const { error, loading, initialize, wasmInstance } = useWasm();
-  const { images } = useImageStore();
-
-  useEffect(() => {
-    async function loadWasm() {
-      await initialize();
-    }
-    loadWasm();
-  }, []);
-
-  if (loading) return <>"Loading..."</>;
-  if (error) {
-    return <>"An error has occurred"</>;
-  }
-
   return (
     <>
       <Header></Header>
@@ -38,17 +20,6 @@ function LandingPage() {
           </h1>
         </motion.div>
         <Dropzone></Dropzone>
-        <button
-          className='mt-8 bg-gray-600 hover:bg-gray-800 dark:bg-gray-400 hover:dark:bg-gray-200 text-white dark:text-gray-100 hover:dark:text-gray-600 py-2 px-4 rounded'
-          onClick={() => {
-            try {
-              wasmInstance?.read_images_web(images[0]);
-            } catch (error) {
-              console.error(error);
-            }
-          }}>
-          Test
-        </button>
       </div>
     </>
   );
